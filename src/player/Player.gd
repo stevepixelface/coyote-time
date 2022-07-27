8ddc0641bc30573b68a989f9c32080a10f1195c4
stevepixelface <stevepixelface@gmail.com>
2022-07-27 18:27:38 +0100

    final tidy before vid tut

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -12,45 +12,46 @@ onready var animated_sprite = $AnimatedSprite
 onready var jump_sound = $JumpSound
 onready var coyote_timer = $CoyoteTimer
 
-func _process(delta):
-	var move_vector = get_movement_vector()
+func _physics_process(delta):
+	var input_vector = get_input_vector()
 	
-	velocity.x += move_vector.x * HORIZONTAL_ACCELERATION * delta
-	if move_vector.x == 0:
+	velocity.x += input_vector.x * HORIZONTAL_ACCELERATION * delta
+	if input_vector.x == 0:
 		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
 		
 	velocity.x = clamp(velocity.x, -MAX_HORIZONTAL_SPEED, MAX_HORIZONTAL_SPEED)
 	
-	if move_vector.y < 0 && (is_on_floor() || !coyote_timer.is_stopped()):
-		velocity.y = move_vector.y * JUMP_SPEED
+	if input_vector.y < 0 && (is_on_floor() || !coyote_timer.is_stopped()):
+		velocity.y = input_vector.y * JUMP_SPEED
 		jump_sound.play()
 
 	if velocity.y < 0 && !Input.is_action_pressed("jump"):
 		velocity.y += GRAVITY * JUMP_TERMINATION_MULTIPLIER * delta
 	else:
 		velocity.y += GRAVITY * delta
-	
+		
 	var was_on_floor = is_on_floor()
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	if was_on_floor && !is_on_floor():
 		coyote_timer.start()
 
-	update_animation(move_vector)
+	update_animation(input_vector)
 	
-func get_movement_vector():
-	var move_vector = Vector2.ZERO
-	move_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	move_vector.y = -1 if Input.is_action_just_pressed("jump") else 0
-	return move_vector
+func get_input_vector():
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = -1 if Input.is_action_just_pressed("jump") else 0
+	return input_vector
 
-func update_animation(move_vector):
+func update_animation(input_vector):
 	if !is_on_floor():
 		animated_sprite.play("jump")
-	elif move_vector.x != 0:
+	elif input_vector.x != 0:
 		animated_sprite.play("run")
 	else:
 		animated_sprite.play("idle")
 	
-	if move_vector.x != 0:
-		animated_sprite.flip_h = true if move_vector.x < 0 else false
+	if input_vector.x != 0:
+		animated_sprite.flip_h = true if input_vector.x < 0 else false
